35ce7a3927128d816c836439125094ddab7d92a1
James Sibert <jamessibert@ssa-417.local>
2016-08-17 12:57:32 -0500

    checks and changes grid space

--- minesweeper.py
+++ minesweeper.py
@@ -2,26 +2,30 @@ def restart():
     ...
 
 
-def is_mine():
-    if "" == '*':
+def is_mine(z):
+    if z == '*':
         input("Game Over. Type 'R' to restart, 'Q' to quit:")
     else:
         return True
 
 
+minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
+
+graygrid = [[None, None, None, None], [None, None, None, None],
+            [None, None, None, None], [None, None, None, None]]
 while True:
 
-    minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
-
-    graygrid = [[None, None, None, None], [None, None, None, None],
-                [None, None, None, None], [None, None, None, None]]
-
+    print('\n' * 30)
     for row in graygrid:
         print(row)
 
     guess = input(
-        "Give X and Y coordinates (x, y). Type 'R' to restart, 'Q' to quit:")
+        """Give X and Y coordinates (x, y). Type 'R' to restart, 'Q' to
+         quit:""").strip().lower().split(',')
     if guess == "r":
         restart()
     elif guess == "q":
         break
+
+    graygrid[guess[1]][guess[0]] = minegrid[guess[1]][guess[0]]
+    is_mine((minegrid[guess[1]][guess[0]]))
