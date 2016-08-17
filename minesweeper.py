dfc61c88c4dadf776af38d4f87133bbdd8a7550a
James Sibert <jamessibert@ssa-417.local>
2016-08-17 14:29:33 -0500

    added info

--- minesweeper.py
+++ minesweeper.py
@@ -1,46 +1,72 @@
+# set f to True
+f = True
+
+
+# restarts the program
 def restart():
     ...
 
 
+# check for a mine
 def is_mine(z):
+
+    # if it's a mine
     if z == '*':
+
+        # print grid and game over
         print('\n' * 30)
         for row in graygrid:
             print(row)
         choose = input("Game Over. Type 'R' to restart, 'Q' to quit: ").strip(
         ).lower()
+
+        # options
         if choose == 'q':
-            return False
+            f = False
+            return f
         elif choose == 'r':
             restart()
         else:
-            return False
+            is_mine(z)
+
+    # if it isn't a mine:
     else:
-        return True
+        f = True
+        return f
 
-
+# the true grid
 minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
 
+# the grid you see at first
 graygrid = [[None, None, None, None], [None, None, None, None],
             [None, None, None, None], [None, None, None, None]]
-while True:
 
+# loop
+while f is True:
+
+    # show grid
     print('\n' * 30)
     for row in graygrid:
         print(row)
 
+    # show instructions
     guess = input(
         """Give X and Y coordinates (x, y). Type 'R' to restart, 'Q' to
 quit: """).strip().lower()
+
+    # options
     if guess == "r":
         restart()
     elif guess == "q":
         break
 
+    # split input into a list
     guess = guess.split(',')
 
+    # set x and y to be what user has input
     x = (int(guess[0]) - 1)
     y = (int(guess[1]) - 1)
 
+    # check for a mine
     graygrid[y][x] = minegrid[y][x]
     is_mine((minegrid[y][x]))
