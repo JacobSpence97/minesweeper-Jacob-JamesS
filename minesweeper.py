ace77745e06002406f7e23402f62efdf62717d4a
James Sibert <jamessibert@ssa-417.local>
2016-08-17 11:20:38 -0500

    added the program

---
+++ minesweeper.py
@@ -0,0 +1,16 @@
+def restart():
+    ...
+
+
+def quit():
+    ...
+
+
+minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
+graygrid = [[None, None, None, None], [None, None, None, None],
+            [None, None, None, None], [None, None, None, None]]
+
+for row in graygrid:
+    print(row)
+# "r" = restart()
+# "q" = quit()
