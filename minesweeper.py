bd03f3a05fa7b5b63ab880b21bdf10f62dd89da7
James Sibert <jamessibert@ssa-417.local>
2016-08-17 11:33:49 -0500

    added input

--- minesweeper.py
+++ minesweeper.py
@@ -2,15 +2,15 @@ def restart():
     ...
 
 
-def quit():
-    ...
-
-
 minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
 graygrid = [[None, None, None, None], [None, None, None, None],
             [None, None, None, None], [None, None, None, None]]
 
 for row in graygrid:
     print(row)
-# "r" = restart()
-# "q" = quit()
+
+guess = input()
+if guess == "r":
+    restart()
+elif guess == "q":
+    break
