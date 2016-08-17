f6d0c93ccf924aee97bff0d3cd5cd472fe826c0a
James Sibert <jamessibert@ssa-417.local>
2016-08-17 11:45:44 -0500

    added function and loop

--- minesweeper.py
+++ minesweeper.py
@@ -2,15 +2,25 @@ def restart():
     ...
 
 
-minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
-graygrid = [[None, None, None, None], [None, None, None, None],
-            [None, None, None, None], [None, None, None, None]]
+def is_mine():
+    if "" == '*':
+        return True
+    else:
+        return False
 
-for row in graygrid:
-    print(row)
 
-guess = input()
-if guess == "r":
-    restart()
-elif guess == "q":
-    break
+while True:
+
+    minegrid = [[1, 1, 1, 0], [1, '*', 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
+
+    graygrid = [[None, None, None, None], [None, None, None, None],
+                [None, None, None, None], [None, None, None, None]]
+
+    for row in graygrid:
+        print(row)
+
+    guess = input()
+    if guess == "r":
+        restart()
+    elif guess == "q":
+        break
